5c013f2975dbf848dd04f20e5e644378db5c1730
whynot-byoul <whynot@hubilon.com>
2024-03-21 17:58:50 +0900

    css 경로 추가(배포오류 수정)

--- src/AppClass.js
+++ src/AppClass.js
@@ -1,5 +1,6 @@
-import React, { Component } from "react";
-import BoxClass from "./component/BoxClass";
+import React, {Component} from 'react';
+import BoxClass from './component/BoxClass';
+import './App.css';
 
 export default class AppClass extends Component {
     constructor(props) {
@@ -16,16 +17,16 @@ export default class AppClass extends Component {
     }
     userSelectHandler = (num) => {
         if (this.state.userCounter === 3 || this.state.comCounter === 3) {
-            alert("삼선승제로 게임이 종료 되었습니다.");
+            alert('삼선승제로 게임이 종료 되었습니다.');
             return false;
         }
         let itemArray = Object.keys(this.choice); // 객체에서 키 값만 뽑아서 어레이로 만들어주는 함수!
         let finalItem = itemArray[num];
 
-        console.log("itemArray", itemArray, "finalItem", finalItem);
+        console.log('itemArray', itemArray, 'finalItem', finalItem);
         const comNum = Math.floor(Math.random() * itemArray.length);
         let comFinalItem = itemArray[comNum];
-        let resultData = "";
+        let resultData = '';
         if (num === 0) {
             // 사용자가 바위
             if (comNum === 0) {
@@ -70,7 +71,7 @@ export default class AppClass extends Component {
                 resultData = 2;
             }
         }
-        console.log("num : ", num, "comNum : ", comNum);
+        console.log('num : ', num, 'comNum : ', comNum);
         this.setState({
             userSelect: this.choice[finalItem],
             comSelect: this.choice[comFinalItem],
@@ -84,43 +85,29 @@ export default class AppClass extends Component {
         });
     };
     choice = {
-        rock: { name: "Rock", icon: "✊" },
-        paper: { name: "Paper", icon: "✋" },
-        scissors: { name: "Scissors", icon: "✌" },
+        rock: {name: 'Rock', icon: '✊'},
+        paper: {name: 'Paper', icon: '✋'},
+        scissors: {name: 'Scissors', icon: '✌'},
     };
     render() {
         return (
-            <div className="body_wrapper">
-                <div className="box_wrap">
-                    <BoxClass
-                        type="user"
-                        selected={this.state.userSelect}
-                        result={this.state.result}
-                    />
-                    <BoxClass
-                        type="com"
-                        selected={this.state.comSelect}
-                        result={this.state.result}
-                    />
+            <div className='body_wrapper'>
+                <div className='box_wrap'>
+                    <BoxClass type='user' selected={this.state.userSelect} result={this.state.result} />
+                    <BoxClass type='com' selected={this.state.comSelect} result={this.state.result} />
                 </div>
-                <div className="counter_wrap">
+                <div className='counter_wrap'>
                     <p>
                         {this.state.userCounter} vs {this.state.comCounter}
                     </p>
                     <button onClick={() => this.reset(0)}>RESET</button>
                 </div>
-                <div className="btn_wrap">
-                    <button onClick={() => this.userSelectHandler(0)}>
-                        ✊
-                    </button>
-                    <button onClick={() => this.userSelectHandler(1)}>
-                        ✋
-                    </button>
-                    <button onClick={() => this.userSelectHandler(2)}>
-                        ✌
-                    </button>
+                <div className='btn_wrap'>
+                    <button onClick={() => this.userSelectHandler(0)}>✊</button>
+                    <button onClick={() => this.userSelectHandler(1)}>✋</button>
+                    <button onClick={() => this.userSelectHandler(2)}>✌</button>
                 </div>
-                <div className="result_wrap"></div>
+                <div className='result_wrap'></div>
             </div>
         );
     }
